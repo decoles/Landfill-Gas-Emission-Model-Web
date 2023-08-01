5cd2ba3d71e14712e6319638f2373945bd9452f2
David Coles <davidepcoles@gmail.com>
2023-08-01 23:39:13 +0000

    got data grid view working

--- src/Component/Inputs/wasteAcceptance.js
+++ src/Component/Inputs/wasteAcceptance.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppContext } from "../../AppContext";
 //Material UI
 import {
@@ -12,32 +12,63 @@ import {
   Radio,
   TextField,
 } from "@mui/material";
-function AcceptanceRates() {
-  const { characteristicsData} = React.useContext(AppContext);
-  const [inputUnits, setInputUnits] = React.useState(0);
+import { DataGrid } from "@mui/x-data-grid";
 
-  const calculatedCalculatedUnits = () => {
-    const {openYear, closeYear} = characteristicsData;
+function AcceptanceRates() {
+  const { characteristicsData } = React.useContext(AppContext);
+  const [inputUnits, setInputUnits] = React.useState(0);
+  const [generatedRows, setGeneratedRows] = useState([]);
+
+
+  const YearCalculation = () => {
+    const { openYear, closeYear } = characteristicsData;
     console.log("open year: " + openYear + " close year: " + closeYear);
-    return (closeYear - openYear);
-  }
+    return closeYear - openYear;
+  };
+
+  const columns = [
+    { field: "year", headerName: "Year", width: 120 },
+    { field: "inputUnits", headerName: "Input Units (Mg/Year)", width: 150 },
+    { field: "calculatedUnits", headerName: "Calculated Units (short tons/year)", width: 180 },
+  ];
+
+
+
+  useEffect(() => {
+    const newRows = [];
+    for (let i = 0; i <= YearCalculation(); i++) {
+      newRows.push({
+        id: i,
+        year: parseInt(characteristicsData.openYear, 10) + parseInt(i, 10),
+        inputUnits: 0,
+        calculatedUnits: 0,
+      });
+    }
+    setGeneratedRows(newRows);
+  }, [characteristicsData.openYear, characteristicsData.closeYear]);
+
+
+
+  
   return (
     <div>
-        <FormControl fullWidth>
+      <FormControl fullWidth>
         <InputLabel id="demo-sidbmple-select-label">Input Units:</InputLabel>
         <Select
-            labelId="demo-sidbmple-select-label"
-            id="demo-simple-select"
-            //value= '' //{age}
-            defaultValue={0}
-            label="Input Units:"
-            //onChange={}
+          labelId="demo-sidbmple-select-label"
+          id="demo-simple-select"
+          //value= '' //{age}
+          defaultValue={0}
+          label="Input Units:"
+          //onChange={}
         >
-            <MenuItem value={0}>Mg/year</MenuItem>
-            <MenuItem value={1}>short tons/year</MenuItem>
+          <MenuItem value={0}>Mg/year</MenuItem>
+          <MenuItem value={1}>short tons/year</MenuItem>
         </Select>
-        </FormControl>
-        <p>Calculated Units: {calculatedCalculatedUnits()}</p>
+      </FormControl>
+      <p>Calculated Units: {YearCalculation()}</p>
+
+      <DataGrid rows={generatedRows} columns={columns} pageSize={5} />
     </div>
   );
 }
